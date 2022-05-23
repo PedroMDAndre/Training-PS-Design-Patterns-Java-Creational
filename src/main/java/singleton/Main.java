c6f3969152171f3e86449e32415ce9a35c48bb4a
PedroMDAndre <pedro.mda@gmail.com>
2022-05-23 23:39:13 +0100

    Singleton

--- src/main/java/singleton/Main.java
+++ src/main/java/singleton/Main.java
@@ -4,13 +4,14 @@ public class Main {
     public static void main(String[] args) {
         singletonTest();
         singletonLazyLoadTest();
-
+        singletonThreadSafeTest();
     }
 
     public static void singletonTest(){
         Singleton singleton1 = Singleton.getInstance();
         Singleton singleton2 = Singleton.getInstance();
 
+        System.out.println("Plain");
         System.out.println("Singleton 1:" + singleton1);
         System.out.println("Singleton 2:" + singleton2 + "\n");
     }
@@ -19,7 +20,17 @@ public class Main {
         SingletonLazyLoad singletonLazyLoad1 = SingletonLazyLoad.getInstance();
         SingletonLazyLoad singletonLazyLoad2 = SingletonLazyLoad.getInstance();
 
+        System.out.println("Lazy Load");
         System.out.println("Singleton 1:" + singletonLazyLoad1);
         System.out.println("Singleton 2:" + singletonLazyLoad2+ "\n");
+    }
+
+    public static void singletonThreadSafeTest(){
+        SingletonThreadSafe singletonThreadSafe1 = SingletonThreadSafe.getInstance();
+        SingletonThreadSafe singletonThreadSafe2 = SingletonThreadSafe.getInstance();
+
+        System.out.println("Thread Safe");
+        System.out.println("Singleton 1:" + singletonThreadSafe1);
+        System.out.println("Singleton 2:" + singletonThreadSafe2+ "\n");
     }
 }
