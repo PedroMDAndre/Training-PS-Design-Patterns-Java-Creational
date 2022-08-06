74d1b2d805f2b66140dd8d7e5e2586b6c68c6b91
PedroMDAndre <pedro.mda@gmail.com>
2022-08-06 22:32:34 +0100

    Factory

--- src/main/java/pt/patterns_with_lambdas/factory/PlayWithFactory.java
+++ src/main/java/pt/patterns_with_lambdas/factory/PlayWithFactory.java
@@ -5,7 +5,6 @@ import pt.patterns_with_lambdas.factory.model.Circle;
 
 import java.awt.*;
 import java.util.List;
-import java.util.function.Supplier;
 
 public class PlayWithFactory {
 
@@ -16,14 +15,12 @@ public class PlayWithFactory {
         Factory<Circle> factory2 = Factory.createFactory(Circle::new);
         Factory<Circle> factory3 = Factory.createFactory(Circle::new);
 
-
         Circle circle = factory1.newInstance();
         System.out.println("Circle = " + circle);
 
         List<Circle> listOfCircles = factory1.create5Objects();
 
         System.out.println("List = " + listOfCircles);
-
 
     }
 
